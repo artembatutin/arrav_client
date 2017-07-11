ce91adf6dddb6901011db5fede908170a060074b
Artem Batutin <artembatutin@gmail.com>
2017-07-10 23:20:11 -0400

    build 27

--- src/net/edge/Constants.java
+++ src/net/edge/Constants.java
@@ -11,7 +11,7 @@ public final class Constants {
 	/*
 	 * Miscellaneous constants
 	 */
-	public static final int BUILD = 26;
+	public static final int BUILD = 27;
 	public static final boolean ANTI_BOT_ENABLED = true;
 	public static final int CACHE_INDEX_COUNT = 8;
 
